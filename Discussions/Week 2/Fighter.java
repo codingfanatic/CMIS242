1f3a2ca34d495ed16f762f0cef7e93a77d653a80
codingfanatic <lclarkerichard@yahoo.com>
2019-10-29 00:24:30 -0400

    Week 2 Discussion - New files Added the java files Person.java and Fighter.java Added pseudocode for both classes

---
+++ Discussions/Week 2/Fighter.java
@@ -0,0 +1,5 @@
+class Fighter extends Person{
+    //Fighter has a weight class, fighting style, and a record
+    //Fighter constructor must invoke Person's constructor
+    //Fighter will override toString() using super and return vals of instance bars with proper labels
+   }
---
+++ Discussions/Week 2/Person.java
@@ -0,0 +1,7 @@
+class Person{
+    private String name, id;
+    private static int idCount = 0;
+    //Person has a display(), and toString()
+    //All instance vars in Person should be private
+    //Person will return vals of instancs vars with proper labels as well as the vals of inherited instance vars using toString()
+   }
