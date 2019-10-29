3c734ea65695cf890207511f46bd2115d0db529e
richard-remote <clarkeril8082@gmail.com>
2019-10-29 10:54:46 -0400

    Noted fixes needed for Person class Made note of the changes required for the Person class Added a file test.java for testing

--- Discussions/Week 2/Person.java
+++ Discussions/Week 2/Person.java
@@ -1,7 +1,12 @@
 class Person{
+    //Id should be an int variable
     private String name, id;
     private static int idCount = 0;
     
+    //Add a constructor which accepts name and id and increments idCount
+
+    //Display method should have the format <<Obj>>>
+    //Only print the object, not Object.toString()
     public void display(){
         System.out.println("<<<Person>>>" + this.toString());
     }
@@ -13,6 +18,8 @@ class Person{
         return this.id;
     }
 
+    //Include specific String formatting
+    //"\n\tId: " + id + name;
     public String toString(){
         return this.toString();
     }
