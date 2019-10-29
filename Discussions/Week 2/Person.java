24bc4a29e4223514bfd1601c1388c26f0f84b1ad
richard-remote <clarkeril8082@gmail.com>
2019-10-29 10:59:02 -0400

    Added some fixes Updated id instance variable to int instead of String Created a contsructor for the Person class which accepts a name, id, and increments the idCount Updated the getId() method to return type of int to match the variable update.

--- Discussions/Week 2/Person.java
+++ Discussions/Week 2/Person.java
@@ -1,10 +1,14 @@
 class Person{
-    //Id should be an int variable
-    private String name, id;
+    private String name;
+    private int id;
     private static int idCount = 0;
     
     //Add a constructor which accepts name and id and increments idCount
-
+    public Person(String name, int id){
+        this.name = name;
+        this.id= id;
+        ++idCount;
+    }
     //Display method should have the format <<Obj>>>
     //Only print the object, not Object.toString()
     public void display(){
@@ -14,7 +18,7 @@ class Person{
         return this.name;
     }
 
-    public String getId(){
+    public int getId(){
         return this.id;
     }
 
