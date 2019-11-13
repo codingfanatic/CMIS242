d40d604e2ddafd9c432cb7b1b1d328596c1fcd30
richard-remote <richard.clarke@socialtables.com>
2019-11-13 15:55:40 -0500

    Added the ButtonPanel Wrote the ButtonPanel class

---
+++ Discussions/Week 4/ButtonPanel.java
@@ -0,0 +1,31 @@
+//Panel for the submission button
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class ButtonPanel extends JPanel {
+    private Formattable fullName;
+    private SimpleNamePanel snPanel;
+    private JButton submitBtn = new JButton("Submit");
+
+    public ButtonPanel(SimpleNamePanel snPanel) {
+        this.snPanel = snPanel;
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+        submitBtn.setToolTipText("Click to submit your names.");
+        add(submitBtn);
+
+        submitBtn.addActionListener(new ActionListener(){
+            public void actionPerformed (ActionEvent e){
+                fullName = new nameLastFirstMiddle();
+                btnFullNameAction();
+            }
+        });
+    }
+
+    public void btnFullNameAction(){
+        String firstName = snPanel.ioPanel.getFirstName();
+        String middleName = snPanel.ioPanel.getMiddleName();
+        String lastName = snPanel.ioPanel.getLastName();
+        snPanel.ioPanel.setFullName(fullName.combineNames(firstName, lastName, middleName));
+    }
+}
