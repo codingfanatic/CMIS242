96dc55e2c27d7bb52fa9737c408d01c678e33080
richard-remote <richard.clarke@socialtables.com>
2019-11-13 16:02:33 -0500

    Added the SimpleNamePanel Now the app should be allllll together!

--- Discussions/Week 4/SimpleNamePanel.java
+++ Discussions/Week 4/SimpleNamePanel.java
@@ -1,4 +1,16 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+
 //The Panel for combining the IOPanel and Buttons Panel
 public class SimpleNamePanel extends JPanel {
-    
+    //Instantiate the IOPanel and Buttons Panel
+    public IOPanel ioPanel = new IOPanel(this);
+    public ButtonsPanel btnPanel = new ButtonsPanel(this);
+
+    public SimpleNamePanel(){
+        setLayout(new BorderLayout());
+        setBackground(Color.lightGray);
+        add(ioPanel, BorderLayout.CENTER);
+        add(btnPanel, BorderLayout.SOUTH);
+    }
 }
