c4aca48b92b7b2e23b2f8f7bbe93d53f236d7797
richard-remote <richard.clarke@socialtables.com>
2019-11-13 13:05:41 -0500

    Finished the Frame constructors Added the default constructor, one which accepts a title, and another which accepts the width and height for a panel.

--- Discussions/Week 4/MyFrame.java
+++ Discussions/Week 4/MyFrame.java
@@ -1,5 +1,29 @@
 import javax.swing.*;
 
 public class MyFrame extends JFrame {
+    //Dimensions for the window
     static final int WIDTH = 500, HEIGHT = 500;
+
+    //Constructor which builds the frame
+    public MyFrame(){
+        super("This is a Frame");
+        setFrame(WIDTH, HEIGHT);
+    }
+
+    public MyFrame(String title){
+        super(title);
+        setFrame(WIDTH, HEIGHT);
+    }
+
+    public MyFrame(String title, int width, int height){
+        super(title);
+        setFrame(width, height);
+    }
+
+    private void setFrame(int width, int height){
+        setSize(width, height);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
 }
---
+++ Discussions/Week 4/SimpleNameApp.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class SimpleNameApp extends MyFrame{
+
+    public SimpleNameApp(){
+        super();
+    }
+
+    public static void main(String[] args){
+        SimpleNameApp snApp = new SimpleNameApp();
+        snApp.display();
+    }
+}
