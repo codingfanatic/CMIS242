823fdcfb56b057842f791eb44b3b7d2d48693fd0
richard-remote <richard.clarke@socialtables.com>
2019-12-04 16:59:58 -0500

    Completed Non-generic class Finished the class which uses a non-generic class

--- Discussions/Week 7/NameArrayNG.java
+++ Discussions/Week 7/NameArrayNG.java
@@ -1,8 +1,8 @@
-class NamesArrayNG{
+class NameArrayNG{
     private Object[] array;
     private int size;
     
-    public NamesArray(){
+    public NameArrayNG(){
         array = new Object[5];
         size = 0;
     }
@@ -18,16 +18,23 @@ class NamesArrayNG{
 
 class TestNamesNG{
     public static void main(String[] args){
-        NameArray<String> array = new NameArray<String>();
-        array.add("Asuma");
-        System.out.println(array.getName(0));
-        array.add("Boruto");
-        System.out.println(array.getName(1));
-        array.add("Ikki");
-        System.out.println(array.getName(2));
-        array.add("Jiraiya");
-        System.out.println(array.getName(3));
-        array.add("A");
-        System.out.println(array.getName(4));
+        NameArrayNG array = new NameArrayNG();
+        array.add(new String("Asuma"));
+        array.add(new String("Boruto"));
+        array.add(new String("Ikki"));
+        array.add(new String("Jiraiya"));
+        array.add(4.12);
+        
+        String first = (String) array.getName(0);
+        String second = (String) array.getName(1);
+        String third = (String) array.getName(2);
+        String fourth = (String) array.getName(3);
+        String fifth = (String) array.getName(4);
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(fourth);
+        System.out.println(fifth);
     }
 }
